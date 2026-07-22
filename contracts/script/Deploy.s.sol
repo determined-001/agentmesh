cfa6a900fe3cd7c8e38fb96a7461c4a5130d82eb
determined-001 <241968004+determined-001@users.noreply.github.com>
2026-07-22 08:29:50 +0100

    chore(contracts): pin pragma 0.8.26 (mock, deploy)

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity 0.8.26;
 
 import {Script, console} from "forge-std/Script.sol";
 import {AgentRegistry} from "../src/AgentRegistry.sol";
