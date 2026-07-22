415a4f0c73c54223b9325afe48369fb9e592e483
determined-001 <241968004+determined-001@users.noreply.github.com>
2026-07-22 08:46:45 +0100

    style: format sync-abis

--- scripts/sync-abis.mjs
+++ scripts/sync-abis.mjs
@@ -72,8 +72,20 @@ const erc20Abi = [
     ],
     outputs: [{ name: "", type: "uint256" }],
   },
-  { type: "function", name: "decimals", stateMutability: "view", inputs: [], outputs: [{ name: "", type: "uint8" }] },
-  { type: "function", name: "symbol", stateMutability: "view", inputs: [], outputs: [{ name: "", type: "string" }] },
+  {
+    type: "function",
+    name: "decimals",
+    stateMutability: "view",
+    inputs: [],
+    outputs: [{ name: "", type: "uint8" }],
+  },
+  {
+    type: "function",
+    name: "symbol",
+    stateMutability: "view",
+    inputs: [],
+    outputs: [{ name: "", type: "string" }],
+  },
   {
     type: "function",
     name: "mint",
